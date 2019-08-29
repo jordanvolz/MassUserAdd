e2ae9beb8fff572dc25c5adf84d3782f13aef2a8
admin <admin>
2019-08-29 10:53:28 -0400

    Edited file 'python-lib/adminfunctions.py'

--- python-lib/adminfunctions.py
+++ python-lib/adminfunctions.py
@@ -5,21 +5,21 @@ def process_file(filepath,skip_header,client,feedback):
         for line in usersfile:
             i+=1
             if (skip_header and i==1): continue
-            printdku("Processing line %s" %line)
+            printdku("Processing line %s" %line,feedback)
             userdetails = line.split(',')
             try: 
                 username = userdetails[0]
                 password = userdetails[1]
                 display_name = userdetails[2]
                 groups = userdetails[3]
-                process_groups(groups)
-                process_user(username,password,display_name,groups)
-                printdku("Successfully processed user %s" %username)
+                process_groups(groups,client,feedback)
+                process_user(username,password,display_name,groups,client,feedback)
+                printdku("Successfully processed user %s" %username,feedback)
             except: 
-                printdku("Error processing line: %s" %line)
+                printdku("Error processing line: %s" %line,feedback)
 
 
-def process_groups(groups,client):
+def process_groups(groups,client,feedback):
     group_list=groups.split("|")
     allgroups = client.list_groups()
     for group in group_list:
@@ -27,11 +27,11 @@ def process_groups(groups,client):
             result = next (group for group in allgroups if group['name'] == group)
         except StopIteration as error: 
             client.create_group(group,group,"LOCAL")
-            printdku("Created group %s" %group)
+            printdku("Created group %s" %group,feedback)
         else: 
-            printdku("Error creating group %s" %group)
+            printdku("Error creating group %s" %group,feedback)
 
-def process_user(username,password,display_name,groups,client):
+def process_user(username,password,display_name,groups,client,feedback):
     #grab user list here instead of before to ensure that we don't process duplicates in the file
     allusers = client.list_users()
     feedback = []
@@ -39,9 +39,9 @@ def process_user(username,password,display_name,groups,client):
         result = next(item for item in allusers if item['login'] == username)
     except StopIteration as error: #user doesn't already exist, create it
         new_user = client.create_user(username, password, display_name,'LOCAL', groups)
-        printdku("Created user %s" %username)
+        printdku("Created user %s" %username,feedback)
     else:
-        printdku("Error creating user %s" %username)
+        printdku("Error creating user %s" %username.feedback)
 
 def printdku(string,feedback):
     print(string)
